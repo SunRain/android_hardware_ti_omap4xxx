9087e680ca9620a9bbdf53526f063a5ce760ee76
SunRain <41245110@qq.com>
2012-09-15 13:19:58 +0800

    libtiutils enable debug tag

--- libtiutils/MessageQueue.h
+++ libtiutils/MessageQueue.h
@@ -23,7 +23,7 @@
 #include <stdint.h>
 
 ///Uncomment this macro to debug the message queue implementation
-//#define DEBUG_LOG
+#define DEBUG_LOG
 
 ///Camera HAL Logging Functions
 #ifndef DEBUG_LOG
